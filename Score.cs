53647c05bef1162fb3ec7ce6227b95372983170d
Alejandra <67431364+Just-Lex@users.noreply.github.com>
2021-04-12 20:43:56 -0500

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,20 @@
+﻿//Nombre: Alejandra Bravo Ayala
+//Materia: Programación Orientada a Objetos
+//Profesor: Josue Israel Rivas Diaz
+//Fuente: Examen bimestral POO 
+//Descripción: Script que ayudará a sumar al score los puntos
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int Puntaje = 20;
+
+    void OnDestroy()
+    {
+        GameController.Score += Puntaje;
+
+    }
+}
